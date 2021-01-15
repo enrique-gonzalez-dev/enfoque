27918625aba99ca1d3930fc2a7fd2081d8f6b363
enriquegonzalez <e.gonzalez@vespatechno.com>
2021-01-14 19:08:25 -0600

    Cambios 14 de enero

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -7,12 +7,10 @@ class Applicant < ApplicationRecord
     has_one :state
     has_one :city
     
-
     validates :name, presence: true
     validates :name, length: { minimum: 10, message: "El nombre es demasiado corto" }
     validates :phone, presence: true
     validates :phone, numericality: { message: "El teléfono solo debe incluir numeros"}
     validates :phone, length: { minimum: 10, message: "El teléfono debe incluir 10 digitos" }
     validates :email, presence: true
-    validates :protest, acceptance: { message: 'Debes declarar que NO estudias y NO trabajas, con el fin de poder ser candidato para el programa de Jóvenes Construyendo el Futuro.' }
 end
